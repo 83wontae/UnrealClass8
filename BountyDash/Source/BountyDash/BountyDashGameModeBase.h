8d9f3d46ce2ae225ed9a665b972b9aeaa6f1d300
junios <junios@naver.com>
2022-10-06 11:31:10 +0900

    aaaa

---
+++ BountyDash/Source/BountyDash/BountyDashGameModeBase.h
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "BountyDash.h"
+#include "GameFramework/GameModeBase.h"
+#include "BountyDashGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOUNTYDASH_API ABountyDashGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY()
+	float gameSpeed;
+
+	UPROPERTY()
+	int32 gameLevel;
+
+public:
+	ABountyDashGameModeBase();
+
+	void CharScoreUp(unsigned int charScore);
+
+	UFUNCTION()
+	float GetInvGameSpeed();
+
+	UFUNCTION()
+	float GetGameSpeed();
+
+	UFUNCTION()
+	int32 GetGameLevel();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 numCoinsForSpeedIncrease;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float gameSpeedIncrease;
+};
